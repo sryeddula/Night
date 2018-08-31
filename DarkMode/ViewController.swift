dce6c14b1a64bb07686792fefdc707014b74d13a
Sudhakar Reddy Yeddula <sryeddula@gmail.com>
2018-08-31 11:28:02 +0100

    Initial Commit

---
+++ DarkMode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DarkMode
+//
+//  Created by Sudhakar Reddy on 31/08/2018.
+//  Copyright © 2018 Avyukta Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
